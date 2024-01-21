e820b50fb41c3575c40197bcef093415f2267030
Char <codream00@gmail.com>
2024-01-22 00:18:19 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,220 @@
+import re
+import json
+import logging
+import time
+import traceback
+
+import av
+from aiortc import RTCPeerConnection, RTCSessionDescription, RTCIceCandidate, RTCConfiguration, VideoStreamTrack
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from scipy.spatial.transform import Rotation
+
+logging.basicConfig(level=logging.WARN)
+
+from src import Camera, GaussianModel, Renderer, get_ice_servers
+
+model_path = "models/dohyun_updown_30000/point_cloud/iteration_30000/point_cloud.ply"
+camera_path = "models/dohyun_updown_30000/cameras.json"
+
+sessions = {}
+
+# load gaussian model
+gaussian_model = GaussianModel().load(model_path)
+
+
+# load camera info
+with open(camera_path, "r") as f:
+    cam_info = json.load(f)[1]
+
+
+# initialize server
+origins = ["*"]
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def parse_frame(container, data):
+    try:
+        packets = container.parse(data)
+        for packet in packets:
+            frames = container.decode(packet)
+            for frame in frames:
+                return frame
+    except Exception as e:
+        logging.error(e)
+        traceback.print_exc()
+
+    return None
+
+
+def create_session(session_id, pc):
+    camera = Camera().load(cam_info)
+    renderer = Renderer(gaussian_model, camera, logging=False)
+    session = Session(session_id, renderer, pc)
+    sessions[session_id] = session
+    return session
+
+
+class Offer(BaseModel):
+    sdp: str
+    type: str
+
+
+class IceCandidate(BaseModel):
+    candidate: str
+    sdpMLineIndex: int
+    sdpMid: str
+    usernameFragment: str
+
+
+class Session:
+    session_id: str
+    renderer: Renderer
+    pc: RTCPeerConnection
+
+    def __init__(self, session_id: str, renderer: Renderer, pc: RTCPeerConnection):
+        self.session_id = session_id
+        self.renderer = renderer
+        self.pc = pc
+
+
+class FrameProducer(VideoStreamTrack):
+    kind = "video"
+
+    def __init__(self, session: Session):
+        super().__init__()
+        self.session = session
+        print(session.renderer.camera)
+
+        container = av.CodecContext.create("h264", "r")
+        container.pix_fmt = "yuv420p"
+        container.width = session.renderer.camera.image_width
+        container.height = session.renderer.camera.image_height
+        container.bit_rate = 14000000
+        container.options = {"preset": "ultrafast", "tune": "zerolatency"}
+
+        self.container = container
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        failed_attempts = 0
+        max_failed_attempts = 10
+
+        while True:
+            try:
+                start_time = time.time()
+                data = self.session.renderer.render()
+                logging.info(f"Render time: {time.time() - start_time}")
+                if data is not None and len(data) > 0:
+                    frame = parse_frame(self.container, data)
+                    if frame is not None:
+                        break
+                    else:
+                        raise Exception("Error parsing frame")
+            except Exception as e:
+                logging.error(e)
+                logging.debug(traceback.format_exc())
+                failed_attempts += 1
+                if failed_attempts >= max_failed_attempts:
+                    logging.error(f"Failed to render frame after {failed_attempts} attempts")
+                    break
+
+        frame.pts = pts
+        frame.time_base = time_base
+
+        return frame
+
+
+@app.post("/ice-candidate")
+async def add_ice_candidate(candidate: IceCandidate, session_id: str = Query(...)):
+    logging.info(f"Adding ICE candidate for session {session_id}")
+    pc = sessions[session_id].pc
+    pattern = r"candidate:(\d+) (\d+) (\w+) (\d+) (\S+) (\d+) typ (\w+)"
+    match = re.match(pattern, candidate.candidate)
+    if match:
+        foundation, component, protocol, priority, ip, port, typ = match.groups()
+        ice_candidate = RTCIceCandidate(
+            component=int(component),
+            foundation=foundation,
+            ip=ip,
+            port=int(port),
+            priority=int(priority),
+            protocol=protocol,
+            type=typ,
+            sdpMid=candidate.sdpMid,
+            sdpMLineIndex=candidate.sdpMLineIndex,
+        )
+        await pc.addIceCandidate(ice_candidate)
+    else:
+        logging.error(f"Failed to parse ICE candidate: {candidate.candidate}")
+
+
+@app.post("/offer")
+async def create_offer(offer: Offer, session_id: str = Query(...)):
+    logging.info(f"Creating offer for session {session_id}")
+    pc = RTCPeerConnection()
+    pc.configuration = RTCConfiguration(iceServers=get_ice_servers())
+
+    session = create_session(session_id, pc,)
+    track = FrameProducer(session)
+    pc.addTrack(track)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        logging.info(f"Connection state: {pc.connectionState}")
+        if pc.connectionState == "failed":
+            await pc.close()
+            del sessions[session_id]
+
+    @pc.on("datachannel")
+    def on_datachannel(channel):
+        @channel.on("message")
+        def on_message(message):
+            payload = json.loads(message)
+            logging.info(f"Received payload: {payload}")
+
+            if payload["type"] == "camera_update":
+                position = payload["position"]
+                rotation = payload["rotation"]
+
+                logging.info(f"Before Euler ${rotation}")
+                rotation = Rotation.from_euler("xyz", rotation, degrees=True).as_matrix()
+                logging.info(f"After Euler ${rotation}")
+                track.session.renderer.update(position, rotation)
+
+    await pc.setRemoteDescription(RTCSessionDescription(sdp=offer.sdp, type=offer.type))
+
+    answer = await pc.createAnswer()
+
+    await pc.setLocalDescription(answer)
+
+    rotation = Rotation.from_matrix(track.session.renderer.camera.cam_info['rotation']).as_euler('xyz',degrees=True)
+    position = track.session.renderer.camera.cam_info['position']
+
+    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type, "cam_info": {"position": position,"rotation":rotation.tolist()}}
+
+
+@app.get("/ice-servers")
+async def get_ice():
+    return get_ice_servers()
+
+
+@app.get("/models", response_class=FileResponse)
+async def download_models():
+    return FileResponse("models/models.zip")
+
+
+# app.mount("/", StaticFiles(directory="gaussian-viewer-frontend/public", html=True), name="public")
+app.mount("/", StaticFiles(directory="realbang-viewer3", html=True), name="realbang-viewer3")
---
+++ realbang-viewer2/client.js
@@ -0,0 +1,278 @@
+import JoyStick from './joy.js'
+import * as THREE from 'https://unpkg.com/three?module';
+
+
+// peer connection
+var pc = null;
+
+//sseionID
+var sessionID = null;
+
+// data channel
+var dc = null, dcInterval = null;
+
+let throttleTimeout = null;
+let joyThrottleTimeout = null;
+
+// control config
+let lastTouchX = 0;
+let lastTouchY = 0;
+let isDragging = false;
+let xDeg = 0;
+let yDeg = 0;
+let offset = new THREE.Vector3(0,0.5,-5)
+let radius = 3;
+
+//joy config
+let joyX = 0;
+let joyY = 0;
+let tempVector = new THREE.Vector3();
+let upVector = new THREE.Vector3(0, 1, 0);
+let previousTimeStamp = null;
+
+const camInfoElement = document.querySelector(".camera-info")
+
+async function init(){
+    const viewer = document.querySelector(".viewer");
+    viewer.addEventListener("mousedown", () => (isDragging = true));
+    viewer.addEventListener("mouseup", () => (isDragging = false));
+    viewer.addEventListener("mousemove", handleMouseMove);
+    viewer.addEventListener("touchstart", handleTouchStart);
+    viewer.addEventListener("touchmove", handleTouchMove);
+    viewer.addEventListener("touchend", () => (isDragging = false));
+
+    new JoyStick('joyDiv', {}, function (stickData) {
+        joyX = stickData.x;
+        joyY = stickData.y;
+    });
+}
+
+async function createPeerConnection() {
+    const iceServers = await fetch(
+        `/ice-servers`,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    ).then((response) => response.json());
+
+    var config = {
+        iceServers: iceServers,
+    };
+
+
+    pc = new RTCPeerConnection(config);
+    pc.addTransceiver("video", { direction: "recvonly" });
+    
+    let iceCandidateBuffer = [];
+    
+    pc.onicecandidate = async ({ candidate }) => {
+        if (candidate) {
+            iceCandidateBuffer.push(candidate);
+        }
+    };
+
+    pc.ontrack = (event) => {
+        console.log("Received track:", event);
+        let player = document.getElementById("player");
+        player.onerror = (error) => {
+            console.error("Error: ", error);
+        };
+        player.srcObject = event.streams[0];
+    };
+
+    dc = pc.createDataChannel("camera");
+    dc.onopen = (event) =>{
+        animate();
+    } 
+    // dc.onmessage = (event) => {
+    //     let data = JSON.parse(event.data);
+    //     camInfoElement.textContent = event.data;
+    // }
+
+
+    const offerOptions = {
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: true,
+    };
+    const offer = await pc.createOffer(offerOptions);
+
+    document.getElementById('offer-sdp').textContent = offer.sdp;
+    await pc.setLocalDescription(offer);
+
+    const answer = await fetch(
+        `/offer?session_id=${sessionID}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                sdp: pc.localDescription.sdp,
+                type: pc.localDescription.type,
+            }),
+        }
+    ).then((response) => response.json());
+    
+    document.getElementById('answer-sdp').textContent = answer.sdp;
+    document.querySelector(".camera-info").innerHTML = 
+    `
+        <p>position: ${answer.cam_info.position}</p>
+        <p>rotation: ${answer.cam_info.rotation}</p>
+    `
+
+    await pc.setRemoteDescription(answer)
+    
+
+}
+
+async function start() {
+    sessionID = Math.random().toString(36).substring(2, 15);
+
+    await createPeerConnection();
+}
+
+function animate(timeStamp) { //position move
+    const angle = THREE.MathUtils.degToRad(-yDeg)
+
+    if(previousTimeStamp && (joyX != 0|| joyY !=0)){
+        var frameTime = timeStamp - previousTimeStamp;
+
+        if(joyX != 0|| joyY !=0){
+            tempVector.set(parseFloat(joyX),0,parseFloat(joyY)).applyAxisAngle(upVector, angle);
+            offset.addScaledVector(
+                tempVector,
+                frameTime * 0.0001
+            );
+        }
+    }
+
+    const position = offset.toArray();
+    const rotation = [-xDeg, -yDeg, 0];
+    throttle(()=>{
+        sendCameraInfoToBackend(position, rotation);
+    },1000/30)
+    requestAnimationFrame( animate );
+    previousTimeStamp = timeStamp;
+}
+
+function stop() {
+    // close data channel
+    if (dc) {
+        dc.close();
+    }
+
+    // close transceivers
+    if (pc.getTransceivers) {
+        pc.getTransceivers().forEach(function(transceiver) {
+            if (transceiver.stop) {
+                transceiver.stop();
+            }
+        });
+    }
+
+    // close local audio / video
+    pc.getSenders().forEach(function(sender) {
+        sender.track.stop();
+    });
+
+    // close peer connection
+    setTimeout(function() {
+        pc.close();
+    }, 500);
+}
+
+function throttle(func, delay) {
+    if (throttleTimeout) {
+        return;
+    }
+    throttleTimeout = setTimeout(() => {
+        func();
+        throttleTimeout = null;
+    }, delay);
+}
+
+function updateCamera(moveX, moveY) {
+    yDeg -= moveX * 0.5;
+    xDeg += moveY * 0.5;
+
+    xDeg = Math.min(Math.max(xDeg, 0), 70);
+    yDeg = (yDeg + 360) % 360;
+
+    // const position = computePosition(xDeg, yDeg);
+    const position = offset.toArray();
+    const rotation = [-xDeg, -yDeg, 0];
+
+    throttle(() => {
+        sendCameraInfoToBackend(position, rotation);
+    }, 1000 / 30);
+}
+
+function computePosition(xDeg, yDeg) {
+    const theta = (xDeg * Math.PI) / 180;
+    const phi = (yDeg * Math.PI) / 180;
+
+    const position = [
+        radius * Math.cos(phi) * Math.cos(theta) + offset[0],
+        -radius * Math.sin(theta) + offset[1],
+        radius * Math.sin(phi) * Math.cos(theta) + offset[2],
+    ];
+
+    return position;
+}
+
+function sendCameraInfoToBackend(position, rotation) {
+    const payload = {
+        type: "camera_update",
+        position,
+        rotation,
+    };
+    camInfoUpdate(position, rotation);
+    dc.send(JSON.stringify(payload));
+}
+
+function camInfoUpdate(position, rotation){
+    document.querySelector(".camera-info").innerHTML = 
+    `
+        <p>position: ${position}</p>
+        <p>rotation: ${rotation}</p>
+    `
+}
+function handleTouchStart(event) {
+    event.preventDefault();
+    isDragging = true;
+    const touch = event.touches[0];
+    lastTouchX = touch.clientX;
+    lastTouchY = touch.clientY;
+}
+
+function handleTouchMove(event) {
+    event.preventDefault();
+    if (!isDragging) return;
+    const touch = event.touches[0];
+    const moveX = touch.clientX - lastTouchX;
+    const moveY = touch.clientY - lastTouchY;
+
+    lastTouchX = touch.clientX;
+    lastTouchY = touch.clientY;
+
+    updateCamera(moveX, moveY);
+}
+
+function handleMouseMove(event) {
+    console.log("touch")
+    if (!isDragging) return;
+    const moveX =
+        event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+    const moveY =
+        event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+    updateCamera(moveX, moveY);
+}
+
+init();
+
+export {start, stop}
+
